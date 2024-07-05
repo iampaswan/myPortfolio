640be661240c1ad3f198bc1efe9cd4b4144f4ccd
Krishna Kumar Paswan <112757024+iampaswan@users.noreply.github.com>
2024-07-05 11:33:57 +0530

    last commit

--- src/Components/Footer.jsx
+++ src/Components/Footer.jsx
@@ -1,12 +1,23 @@
 import styles from './Footer.module.css'
 
+const instaLink = "https://www.instagram.com/iampaswanx?igsh=NDg2c2NjeDZoeXdu"
+
 const Footer = () => {
   return <>
     <div className={styles.Footer}>
 
-      <div className={styles.leftFooter}>Whatsapp</div>
-      <div className={styles.middleFooter}>Email</div>
-      <div className={styles.rightFooter}>Follow me</div>
+      <div className={styles.leftFooter}>
+        <div>Whatsapp</div>
+        <div className={styles.FooterButtons}><i className="bi bi-whatsapp"></i></div>
+      </div>
+      <div className={styles.middleFooter}>
+        <div>Email</div>
+        <div className={styles.FooterButtons}><i className="bi bi-envelope"></i></div>
+      </div>
+      <div className={styles.rightFooter}>
+        <div>Follow me</div>
+        <div className={styles.FooterButtons}> <a href={instaLink}><i href="" className="bi bi-instagram"></i></a></div>
+      </div>
 
     </div>
   </>
