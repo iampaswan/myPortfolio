267f6403421368a82057cc71656422e11c0dfc3f
iampaswan <krishna1119kumar@gmail.com>
2024-07-08 18:55:54 +0530

    lase

--- src/Components/About.jsx
+++ src/Components/About.jsx
@@ -3,7 +3,7 @@ import styles from './About.module.css'
 const About = () => {
   return <>
     <div className={styles.About}>
-      About
+      I am Krishna Kumar Paswan. I was born on 1st September 2003 in Balli Uttar Pradesh.
     </div>
   </>
 }
